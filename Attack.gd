0db57b2fd79e7b54e4d4ab7186a8da6f046ec304
Robbin Ploeger <robbplo@gmail.com>
2024-06-09 14:36:05 +0200

    add enemy health pool

--- Attack.gd
+++ Attack.gd
@@ -1,20 +1,29 @@
 extends CharacterBody2D
 
+signal attack_start
+signal attack_end
+signal attack_hit
+
 var is_attacking = false
 
-## Returns whether the attack was started or not
 func start(target: Vector2):
 	if is_attacking:
 		return false
+	emit_signal("attack_start")
 	self.look_at(target)
 	is_attacking = true
-	show()
-	$Area2D.monitoring = true
+	$Animation.show()
 	$Animation.play()
+	
+	for body in $Area2D.get_overlapping_bodies():
+		if not body.is_ancestor_of(self):
+			emit_signal("attack_hit", body)
+	
 	return true
 
 func _on_animation_animation_looped():
+	emit_signal("attack_end")
 	is_attacking = false
-	hide()
-	$Area2D.monitoring = false
+	$Animation.hide()
 	$Animation.stop()
+
