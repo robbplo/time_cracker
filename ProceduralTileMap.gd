3453572bb3a6f5aa738369ffa17551ee0e181f9e
JonSewi <j.waard@live.com>
2024-06-09 20:16:32 +0200

    Changed background colors and pattern, added NoIntroSong

--- ProceduralTileMap.gd
+++ ProceduralTileMap.gd
@@ -1,10 +1,14 @@
+@tool
+
 extends TileMap
 
 var rng = RandomNumberGenerator.new()
 
 var _k = 0
 var _lo = 0
+var _mid = 0
 var _hi = 0
+var _empty = 0
 
 func _ready():
 	rng.randomize()
@@ -15,14 +19,20 @@ func _ready():
 	for _i in range(200):
 		for _j in range (0,300):
 			_k = _noise.get_noise_2d(_i, _j)
-			if  _k < -0.2 :
-				set_cell(0,Vector2i(_i,_j), 0, Vector2i(0,0))
+			if  _k < -0.5 :
+				set_cell(0,Vector2i(_i,_j), 0, Vector2i(randf_range(-1,7),randf_range(-1,7)))
 				_lo += 1
-			elif _k > 0.3:
-				set_cell(0,Vector2i(_i,_j,), 0, Vector2i(0,1))
-				_hi += 1
+			elif _k > -0.5 && _k <0:
+				set_cell(0,Vector2i(_i,_j,), 1, Vector2i(randf_range(-1,7),randf_range(-1,7)))
+				_mid += 1
+			elif _k > 0 && _k < 0.4:
+				set_cell(0,Vector2i(_i,_j,), 2, Vector2i(randf_range(-1,7),randf_range(-1,7)))
+				_hi += 1	
 			else:
-				set_cell(0, Vector2i(_i, _j,), 0, Vector2i(0,2))
-								
+				set_cell(0, Vector2i(_i, _j,), -1, Vector2i(randf_range(-1,7),randf_range(-1,7)))
+				_empty += 1
 	print(_lo)
+	print(_mid)
 	print(_hi)
+	print(_empty)
+	
