02d1e662197dd0ccd440553b3f61f6e4afd99ac5
JonSewi <j.waard@live.com>
2024-07-01 16:12:29 +0200

    Magic circles now use inputs! good shit.

--- Experiments/Magic/Magic.gd
+++ Experiments/Magic/Magic.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 
 const BULLET = preload("res://Experiments/Magic/bullet.tscn")
+@onready var MCircleComp: Area2D = $SubViewportContainer/SubViewport/MagicCircle
+@onready var MCircle1: AnimationPlayer = $SubViewportContainer/SubViewport/MagicCircle/MCircles
+@onready var MCircle2: AnimationPlayer = $SubViewportContainer/SubViewport/MagicCircle/MCircles_2
+@onready var MCircle3: AnimationPlayer = $SubViewportContainer/SubViewport/MagicCircle/MCircles_3
+@onready var MCircleSprites1: Sprite2D = $SubViewportContainer/SubViewport/MagicCircle/MCircleSheets_layer1
+@onready var MCircleSprites2: Sprite2D = $SubViewportContainer/SubViewport/MagicCircle/MCircleSheets_layer2
+@onready var MCircleSprites3: Sprite2D = $SubViewportContainer/SubViewport/MagicCircle/MCircleSheets_layer3
 
 var spell_components: = []
 var is_casting = false
 
-var mCircle_currentFrame = 0
-var mCircle_toFrame = 0
-
 func _ready():
-	pass
+	reset_MCircle()
 
 func _process(_delta):
 	if not is_casting:
 		move_and_slide()
+		reset_MCircle()
 	else:
 		if Input.is_action_just_pressed("left"):
 			add_component("left")
@@ -38,12 +43,16 @@ func add_component(component):
 		return
 	spell_components.append(component)
 	$RichTextLabel.text = ", ".join(spell_components)
+	update_MCircle(component)
 
 ## just fookin send it
 func send_it():
 	spell_components.clear()
 	$RichTextLabel.text = ""
 	fire_bullet()
+	reset_MCircle()
+	
+	
 
 ## Hell yea brother
 func fire_bullet():
@@ -52,7 +61,27 @@ func fire_bullet():
 	bullet.global_position = global_position
 	get_tree().root.add_child(bullet)
 	
+func update_MCircle(component):
+	if (spell_components.size() == 1):
+		MCircle1.play(component)
+		MCircleSprites1.visible = true
+	elif (spell_components.size() == 2):
+		MCircle2.play(component)
+		MCircleSprites2.visible = true
+	elif (spell_components.size() == 3):
+		MCircle3.play(component)
+		MCircleSprites3.visible = true
+	
+func reset_MCircle():
+	MCircle1.stop()
+	MCircleSprites1.visible = false
+	MCircle2.stop()
+	MCircleSprites2.visible = false
+	MCircle3.stop()
+	MCircleSprites3.visible = false
 
+	
+	
 # force pushu
 # (slightly) houming projectiles
 # big rock that follows cursor
